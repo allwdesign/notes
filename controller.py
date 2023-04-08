86d5445c95083ef1b0fec0feafccf8e86d0b0d97
Natalia Kuzmenko <allwdesign@gmail.com>
2023-04-08 20:11:16 +0300

    Add controller module

---
+++ controller.py
@@ -0,0 +1,19 @@
+""" This is the controller module.
+
+Accepts user input commands and delegates the presentation of the data to the
+view, and the handling of the data to the model.
+"""
+
+COMMANDS = ['add', 'list', 'read', 'edit', 'del', 'exit', 'help', 'save']
+
+
+def execute_command(command):
+    pass
+
+
+def run():
+    """
+    """
+    # Getting data with notes from a file
+    # We try to execute the command received from the user
+    pass
